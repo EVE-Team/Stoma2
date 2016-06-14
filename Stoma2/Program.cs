3ba2fb8cd63819b0614e888b17e197a4690d4c13
mark-korotkov <mkgenz95@gmail.com>
2016-06-14 21:25:33 +0300

    Allow only one application instance

--- Stoma2/Program.cs
+++ Stoma2/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace Stoma2
 {
@@ -12,12 +15,35 @@ namespace Stoma2
 		public static MainForm mainForm;
 		public static string backupToRestore = null;
 
+		[DllImport("user32.dll")]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		static extern bool SetForegroundWindow(IntPtr hWnd);
+
+		static private bool IsOtherInstanceOpen()
+		{
+			Process current = Process.GetCurrentProcess();
+			foreach (Process process in Process.GetProcessesByName(current.ProcessName))
+			{
+				if (process.Id != current.Id)
+				{
+					SetForegroundWindow(process.MainWindowHandle);
+					return true;
+				}
+			}
+			return false;
+		}
+
         /// <summary>
 		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
 		static void Main()
 		{
+			if (IsOtherInstanceOpen())
+			{
+				return;
+			}
+
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 
